c48db203327be89dac84861de765e7d314214d93
Renas Said <renas.said@live.com>
2022-06-22 13:27:55 +0200

    Vehivle file

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,3 +1,12 @@
-public class Vehicle {
+public abstract class Vehicle {
+    String regNr;
     
+    public abstract int getFee();
+    public abstract String getType();
+    
+    protected Vehicle(String regNr) {
+        this.regNr = regNr;
+    }
+    
+
 }
