41ad388ea9ae4cf80ee36dd8c1b9179193e8d735
Renas Said <renas.said@live.com>
2022-06-22 13:16:52 +0200

    Main file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        TollCalculator tollCalculator = new TollCalculator();
+
+        Vehicle car = new Car("xtv123");
+        Vehicle mc = new Motorbike("rqt394");
+        
+        tollCalculator.getTotalTollFee(car, TimeStamps.getCamera3());
+        tollCalculator.getTotalTollFee(mc, TimeStamps.getCamera1());
+        tollCalculator.getTotalTollFee(car, TimeStamps.getCamera2());
+
+
     }
 }
