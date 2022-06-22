14af1360097717d1fa8c56fe2a579339f55cd328
Renas Said <renas.said@live.com>
2022-06-22 13:08:12 +0200

    "Car file"

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,15 @@
-public class Car {
+public class Car extends Vehicle {
+    public static final String TYPE = "CAR";
     
+    public Car(String regNr) {
+        super(regNr);
+    }
+
+    public int getFee() {
+        return 11;
+    }
+    
+    public String getType() {
+        return TYPE;
+    }
 }
