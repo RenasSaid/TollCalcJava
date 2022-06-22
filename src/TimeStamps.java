3144e3a4b4ac46afa2ec5ea69ff8800d9ca77dc6
Renas Said <renas.said@live.com>
2022-06-22 13:23:52 +0200

    TimeStamps file

--- src/TimeStamps.java
+++ src/TimeStamps.java
@@ -1,3 +1,49 @@
+import java.time.LocalDateTime;
+
 public class TimeStamps {
-    
+
+    // kamera för tidpunkter under RushHour och normala tider. Totalen överskrider 60kr.
+    private static LocalDateTime[] camera1() {
+        LocalDateTime datetime1 = LocalDateTime.of(2021, 11, 30, 7, 10, 19);
+        LocalDateTime datetime2 = LocalDateTime.of(2021, 11, 30, 16, 59, 9);
+        LocalDateTime datetime3 = LocalDateTime.of(2021, 11, 30, 16, 30, 1);
+        LocalDateTime datetime4 = LocalDateTime.of(2021, 11, 30, 17, 30, 1);
+        LocalDateTime datetime5 = LocalDateTime.of(2021, 11, 30, 8, 10, 19);
+        LocalDateTime datetime6 = LocalDateTime.of(2021, 11, 30, 7, 10, 19);
+
+        LocalDateTime[] dates = {datetime1, datetime2, datetime3, datetime4, datetime5, datetime6};
+        return dates;
+    }
+    // kamera för tidpunkter på helgdag.
+    private static LocalDateTime[] camera2() {
+        LocalDateTime datetime1 = LocalDateTime.of(2021, 12, 4, 10, 10, 43);
+        LocalDateTime datetime2 = LocalDateTime.of(2021, 12, 4, 11, 15, 9);
+        LocalDateTime datetime3 = LocalDateTime.of(2021, 12, 4, 14, 31, 9);
+        LocalDateTime datetime4 = LocalDateTime.of(2021, 12, 4, 15, 47, 11);
+
+        LocalDateTime[] dates = {datetime1, datetime2, datetime3, datetime4};
+        return dates;
+
+    }
+    // kamera för tidpunkter utanför rushHour.
+    private static LocalDateTime[] camera3() {
+        LocalDateTime datetime1 = LocalDateTime.of(2021, 12, 2, 18, 23, 13);
+        LocalDateTime datetime2 = LocalDateTime.of(2021, 12, 2, 19, 2, 41);
+
+        LocalDateTime[] dates = {datetime1, datetime2};
+        return dates;
+    }
+
+    // Getters.
+    public static LocalDateTime[] getCamera1() {
+        return camera1();
+    }
+
+    public static LocalDateTime[] getCamera2() {
+        return camera2();
+    }
+
+    public static LocalDateTime[] getCamera3() {
+        return camera3();
+    }
 }
