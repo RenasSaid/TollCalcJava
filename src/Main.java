0109b644e425cc18d07a619333abb900f4eaab3a
Renas Said <renas.said@live.com>
2022-06-23 12:43:17 +0200

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         TollCalculator tollCalculator = new TollCalculator();
 
-        Vehicle car = new Car("xtv123");
-        Vehicle mc = new Motorbike("rqt394");
+        Vehicle car = new Car("UMF422");
+        Vehicle mc = new Motorbike("AKT153");
         
         tollCalculator.getTotalTollFee(car, TimeStamps.getCamera3());
         tollCalculator.getTotalTollFee(mc, TimeStamps.getCamera1());
--- src/TollCalculatorTest.java
+++ src/TollCalculatorTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 
 public class TollCalculatorTest {
     TollCalculator tollCalculator = new TollCalculator();
-    Vehicle car = new Car("bte394");
-    Vehicle mc = new Motorbike("trq290");
+    Vehicle car = new Car("PMJ770");
+    Vehicle mc = new Motorbike("MTJ644");
     LocalDateTime nonRushHour = LocalDateTime.of(2021, 12, 6, 10, 10, 19);
     LocalDateTime morningRushHour = LocalDateTime.of(2021, 12, 6, 8, 10, 19);
     LocalDateTime afternoonRushHour = LocalDateTime.of(2021, 12, 6, 16, 30, 0);
