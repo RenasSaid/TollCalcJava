8b59b8ccf19231015d55edd0e1849b61fa341763
Renas Said <renas.said@live.com>
2022-06-23 12:33:50 +0200

    Added Timestapms, TollCalc and Test files

--- src/TollCalculatorTest.java
+++ src/TollCalculatorTest.java
@@ -1,3 +1,57 @@
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TollCalculatorTest {
+    TollCalculator tollCalculator = new TollCalculator();
+    Vehicle car = new Car("bte394");
+    Vehicle mc = new Motorbike("trq290");
+    LocalDateTime nonRushHour = LocalDateTime.of(2021, 12, 6, 10, 10, 19);
+    LocalDateTime morningRushHour = LocalDateTime.of(2021, 12, 6, 8, 10, 19);
+    LocalDateTime afternoonRushHour = LocalDateTime.of(2021, 12, 6, 16, 30, 0);
+    LocalDateTime[] weekend = {LocalDateTime.of(2021, 12, 5, 9, 15, 44)};
+
+
+    // Test av normaltaxa av bil samt MC
+    // Bilar kostar normalt 11 kr för att passera vägkamera
+    // Motorcyklar kostar normalt 8 kr för att passera vägkamera
+    @Test
+    public void testVechileFee() {
+
+        Assertions.assertEquals(11, car.getFee());
+        Assertions.assertEquals(8, mc.getFee());
+
+    }
+
+    // Test av helgtaxering för bil samt MC
+    // Lördagar och Söndagar är avgiftsfria
+    @Test
+    public void testWeekendFee() {
+
+        Assertions.assertEquals(0, tollCalculator.getTotalTollFee(car, weekend));
+        Assertions.assertEquals(0, tollCalculator.getTotalTollFee(mc, weekend));
+
+    }
+
+    // Test av maxtaxering för bil samt MC
+    // Rusningstrafik mellan (8:00 - 9:00) och (16:00-17:00) ger maxtaxa (18 kr)
+    @Test
+    public void testRushHourFee() {
+        LocalDateTime[] rushHours = {morningRushHour, afternoonRushHour};
+
+        Assertions.assertEquals(2*18, tollCalculator.getTotalTollFee(car, rushHours));
+        Assertions.assertEquals(2*18, tollCalculator.getTotalTollFee(mc, rushHours));
+    }
     
+    // Test av maxtaxering för bil samt MC
+    // Högsta dagliga avgiften får aldrig överskrida 60 kr
+    @Test
+    public void testTotalMaxFee() {
+        LocalDateTime[] mix = {nonRushHour, morningRushHour, nonRushHour, afternoonRushHour, nonRushHour, nonRushHour};
+        
+        Assertions.assertEquals(60, tollCalculator.getTotalTollFee(car, mix));  //Taxa för bil utan maxtak är 76kr
+        Assertions.assertEquals(60, tollCalculator.getTotalTollFee(mc, mix));   //Taxa för MC utan maxtak är 68kr
+        
+    }
+
 }
